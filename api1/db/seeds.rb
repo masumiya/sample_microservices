4c184c6fbd6ffa1004401b50f5586fd2939f86cf
Yuichi Masumiya <github@wakizashi.jp>
2016-06-19 13:17:16 +0900

    add execution of calling api.

--- api1/db/seeds.rb
+++ api1/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Member.delete_all
+Member.create(name: 'Person1', age: 20)
+Member.create(name: 'Person2', age: 25)
+Member.create(name: 'Person3', age: 30)
+Member.create(name: 'Person4', age: 35)
+Member.create(name: 'Person5', age: 40)
