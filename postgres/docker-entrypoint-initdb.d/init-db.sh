d545b8c73c9741f0ad9dfee7bc5f9a9c9eebbf19
Yuichi Masumiya <github@wakizashi.jp>
2016-07-03 13:15:40 +0900

    add db ownership for api1

--- postgres/docker-entrypoint-initdb.d/init-db.sh
+++ postgres/docker-entrypoint-initdb.d/init-db.sh
@@ -8,4 +8,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   ALTER ROLE api1_user CREATEDB;
   CREATE DATABASE api1_db;
   GRANT ALL PRIVILEGES ON DATABASE api1_db TO api1_user;
+  ALTER DATABASE api1_db OWNER TO api1_user;
 EOSQL
